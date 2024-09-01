301d187531535ac756d0936a93ce36d3254e35d6
Miguel Vila <miviro0804@gmail.com>
2024-09-01 02:59:19 +0200

    js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello, world!');
+const chokidar = require('chokidar');
+const fs = require('fs');
+const path = require('path');
+
+const watcher = chokidar.watch('user', {
+  persistent: true
+});
+
+watcher.on('change', (filePath) => { // TODO: on change and on add and on delete 
+  const fileName = path.basename(filePath);
+  /*const destination = path.join('/path/to/destination', fileName);
+  fs.copyFile(filePath, destination, (err) => {
+    if (err) throw err;
+    console.log(`${fileName} was copied to ${destination}`);
+  });*/
+    console.log(`${fileName} was changed`);
+});
